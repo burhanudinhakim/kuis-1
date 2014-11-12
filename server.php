b05b79820ccee6e4dbd8db38443aa14fd87010fc
feradwil <fera.dwi.l@mail.ugm.ac.id>
2014-11-12 08:30:57 +0700

    add

--- server.php
+++ server.php
@@ -36,4 +36,35 @@
     }
     //create HTTP listener
     $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : ''; $server->service($HTTP_RAW_POST_DATA);
+	
+	
+	function simpan_daftar($email, $nim, $nama, $jenis_kelamin, $alamat) {
+	
+	$db = NewADOConnection('mysql');
+      $db -> Connect('128.199.182.167','kuis1','rahasia','kuis1'); 
+      $sql = $db -> Execute("SELECT * FROM pengguna where email='$email' nim='$nim' nama='$nama' jenis_kelamin='$jenis_kelamin' alamat='$alamat'");
+   
+
+	$data = array('email'=>$this->input->post('email'),
+				  'nim'=>$this->input->post('nim'),
+				  'nama'=>$this->input->post('nama'),
+				  'nim'=>$this->input->post('nim'),
+				  'jenis_kelamin'=>$this->input->post('jenis_kelamin'),
+				  'alamat'=>$this->input->post('alamat')
+				
+				  );
+	
+	if($result == 1)     { 
+	echo ‘ERROR! The username you have chosen already exists!’;     
+	} else     {     
+	mysql_query("INSERT INTO TABLENAME (username, password, firstname, lastname, age, ip) 
+	VALUES ('$username', '$password', '$firstname', '$lastname', '$age', '$ip')");    
+	echo '   
+ <p> Congratulations! You have successfully registered! Click here to login.‘; </p>
+
+}
+
+	
+	
+	
 ?>
