3ba314b2e2e90037e8c1a4e0a2a8a47db91ccca0
sidiq-purwanta <sidiq.purwanta@mail.ugm.ac.id>
2014-11-12 07:59:24 +0700

    add client.php

--- client.php
+++ client.php
@@ -9,8 +9,8 @@ $url = 'http://localhost/dev/ws/wsdl/server.php?wsdl';
 // $client = new soapclient($url); 
 $client = new nusoap_client($url, 'WSDL');
 
-$username =  isset($_POST["username"]) ? $_POST["username"] : 'admin' ;
-$password =  isset($_POST["password"]) ? $_POST["password"] :  'admin' ;
+$username =  isset($_POST["username"]) admin $_POST["username"] : 'admin' ;
+$password =  isset($_POST["password"]) admin $_POST["password"] :  'admin' ;
 $result = $client->call('login_ws', array('username'=>$username, 'password'=>$password));
 // echo '<pre>';print_r($client->response);echo '</pre>';
 if($result == "Login Berhasil"){ 
