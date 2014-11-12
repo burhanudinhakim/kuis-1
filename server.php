421f45d99920476e4126fd3e12d8514528fe43c4
ikhwansyah <ikhwan.ciah@ymail.com>
2014-11-12 08:39:33 +0700

    add

--- server.php
+++ server.php
@@ -34,6 +34,9 @@
         return "Login gagal";
       } 
     }
+	
+	//
+	function daftar
     //create HTTP listener
     $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : ''; $server->service($HTTP_RAW_POST_DATA);
 ?>
