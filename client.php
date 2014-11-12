4c46d71746bef9d4d2a17f4aa68a6f7b9929d4ff
cephisinta <cephisinta@gmail.com>
2014-11-12 08:29:30 +0700

    Kuis 1

--- client.php
+++ client.php
@@ -5,7 +5,7 @@ session_start();
 //panggil library
 require_once('nusoap/lib/nusoap.php');
 //mendefinisikan alamat url service yang disediakan oleh client 
-$url = 'http://localhost/dev/ws/wsdl/server.php?wsdl';
+$url ='http://localhost/kuis-1.git/server.php?wsdl';
 // $client = new soapclient($url); 
 $client = new nusoap_client($url, 'WSDL');
 
@@ -15,7 +15,7 @@ $result = $client->call('login_ws', array('username'=>$username, 'password'=>$pa
 // echo '<pre>';print_r($client->response);echo '</pre>';
 if($result == "Login Berhasil"){ 
   $_SESSION['username'] = $username; 
-  header ("location:index.php");
+  header ("location:registrasi.php");
 } else{
   header ("location:login.php"); 
 }
