a3dc9d1b7c2ce1b632ec5e829114085249e6ff3d
diankurnia <diankurniaputri5@gmail.com>
2014-11-19 08:42:53 +0700

    add new

--- server_registrasi_328243.php
+++ server_registrasi_328243.php
@@ -25,11 +25,7 @@
     function registrasi_ws($email, $nama, $nim, $jenis_kelamin, $alamat) {
       $db = NewADOConnection('mysql');
       $db->Connect('128.199.182.167','kuis1','rahasia','kuis1'); 
-      $email = mysql_escape_string($email);
-      $nim = mysql_escape_string($nim);
-      $nama = mysql_escape_string($nama);
-      $jenis_kelamin = mysql_escape_string($jenis_kelamin);
-      $alamat = mysql_escape_string($alamat);
+
       $sql = $db->Execute("SELECT * FROM pengguna where email='$email'");
       $sql2 = $db->Execute("SELECT * FROM pengguna where nim='$nim'");
       if ($sql->RecordCount() >= 1 || $sql2->RecordCount() >= 1)
