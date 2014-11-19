4b21083fd848fbc79ee528a28ab8e8d754364a4d
indargo <indargo.budi.p@mail.ugm.ac.id>
2014-11-19 07:40:12 +0700

    Update client_registrasi_00866.php

--- client_registrasi_00866.php
+++ client_registrasi_00866.php
@@ -2,20 +2,22 @@
 error_reporting(E_ALL);
 ini_set('display_error',1);
 session_start();
-//panggil library
 require_once('nusoap/lib/nusoap.php');
-//mendefinisikan alamat url service yang disediakan oleh client 
-$url = 'http://localhost/dev/ws/wsdl/server.php?wsdl';
-// $client = new soapclient($url); 
+$url = 'http://localhost/dev/kuis-1/server_registrasi_jawaban.php?wsdl';
 $client = new nusoap_client($url, 'WSDL');
-
-$email	= isset($_POST["Email"]) ? $_POST["Email"] : '';
-$nim	= isset($_POST["Nim"]) ? $_POST["Nim"] : '';
-$nama	= isset($_POST["Nama"]) ? $_POST["Nama"] : '';
-$jenis_kelamin	= isset($_POST["Jenis kelamin"]) ? $_POST["Jenis kelamin"] : '';
-$alamat	= isset($_POST["Alamat"]) ? $_POST["Alamat"] : '';
-
-$result = $client->call('Registrasi', array('email'=>$email, 'nim'=>$nim, 'nama'=>$nama, 'jenis kelamin'=>jenis_kelamin, 'alamat'=>alamat));
-// echo '<pre>';print_r($client->response);echo '</pre>';
-
+$p_post = $_POST;
+$result = $client->call('registrasi_ws', 
+  array(
+  'email'         =>  $p_post['email'], 
+  'nama'          =>  $p_post['nama'], 
+  'nim'           =>  $p_post['nim'], 
+  'jenis_kelamin' =>  $p_post['jenis_kelamin'], 
+  )
+);
+if($result == "Registrasi Berhasil"){ 
+  $_SESSION['email'] = $p_post['email']; 
+  header ("location:index_registrasi_berhasil.php");
+} else{
+  header ("location:form_registrasi_jawaban.php"); 
+}
 ?>
