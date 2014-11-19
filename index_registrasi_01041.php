ef8c720f9fec016ba299be0ba22c2fd54716b8c9
istibudiningtyas <isti.budiningtyas@mail.ugm.ac.id>
2014-11-19 08:49:30 +0700

    remidi_blm_fix

--- index_registrasi_01041.php
+++ index_registrasi_01041.php
@@ -4,10 +4,10 @@
   //memulai session
   session_start();
   //cek adanya session
-  if(ISSET($_SESSION['username'])){
+  if(ISSET($_SESSION['email'])){
     //jika tidak ada session 
   } else
-  header("location:login.php");
+  header("location:form_registrasi_01041.php");
 ?>
   <link href="asset/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="asset/css/navbar-fixed-top.css" rel="stylesheet" type="text/css">
@@ -32,8 +32,6 @@
             <li class="active"><a href="#">Home</a></li>
           </ul> 
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar-static-top/">Anda login sebagai <?php echo $_SESSION['username'];?></a></li>
-            <li class="active"><a href="logout.php">Logout</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -43,7 +41,7 @@
 
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <h3>Selamat Datang di ELISA Universitas Gadjah Mada</h3>
+        <h3>Registrasi Pengguna Berhasil</h3>
         <p>Mari berbagi pengetahuan, berkolaborasi dan menjadi produktif dalam kerangka pembelajaran.</p>
         <p>
           <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Selengkapnya &raquo;</a>
