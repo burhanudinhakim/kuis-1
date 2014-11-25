f17bace6502cd917ada087885fe76c5f206048f1
novalindadesi <novalinda.desi.r@mail.ugm.ac.id>
2014-11-25 14:00:45 +0700

    remidi kuis1

--- client_registrasi_332449.php
+++ client_registrasi_332449.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_error',1);
 session_start();
 require_once('nusoap/lib/nusoap.php');
-$url = 'http://localhost/kuis-1.git/server_registrasi_332449.php?wsdl';
+$url = 'http://localhost/kuis-1/server_registrasi_332449.php?wsdl';
 $client = new nusoap_client($url, 'WSDL');
 $p_post = $_POST;
 $result = $client->call('register_ws', 
